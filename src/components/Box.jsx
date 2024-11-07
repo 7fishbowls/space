9f6a323e16ec623b12a1dfd5ed360edcda8630a1
BOOBS <7fishbowls@gmail.com>
2024-11-07 08:45:01 +0530

    Temp2

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -1,8 +1,12 @@
+import { FcCheckmark } from "react-icons/fc";
 function Box({ title, body }) {
   return (
     <section className="about_box">
       <header>
-        <h2>{title}</h2>
+        <h2>
+          <FcCheckmark className="about_icons" />
+          {title}
+        </h2>
       </header>
       <section className="content">
         <p>{body}</p>
